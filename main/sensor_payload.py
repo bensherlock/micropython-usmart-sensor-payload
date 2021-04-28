1aaf3b9b3da65ff8460fc1c823e59f85862aaaee
Ben Sherlock <benjamin.sherlock@ncl.ac.uk>
2021-04-28 16:26:41 +0100

    Added calibration function for magnetometer.

--- main/sensor_payload.py
+++ main/sensor_payload.py
@@ -30,7 +30,8 @@
 #
 """MicroPython USMART Sensor Payload."""
 
-# Standard Interface for Sensor Payloads for Gateway and Sensor Nodes. 
+
+# Standard Interface for Sensor Payloads for Gateway and Sensor Nodes.
 # For nodes with different sensors available, implement this api and provide your data accordingly.
 
 class SensorPayload:
@@ -78,9 +79,9 @@ from pybd_expansion.main.bme280 import BME280
 import pybd_expansion.main.lsm303agr as lsm303agr
 from pybd_expansion.main.lsm303agr import LSM303AGR
 import pyb
+import utime
 
 import struct
-
 
 
 class PebSensorPayload(SensorPayload):
@@ -107,7 +108,6 @@ class PebSensorPayload(SensorPayload):
         self._lsm303agr_magneto = None
 
         self._powermodule_vbatt = None
-
 
         pass
 
@@ -152,7 +152,8 @@ class PebSensorPayload(SensorPayload):
 
         # Setup to take measurements
         self._bme280.set_ctrl_hum_reg(osrs_h=bme280.OSRS_OVERSAMPLE_X_1)
-        self._bme280.set_ctrl_meas_reg(mode=bme280.MODE_NORMAL, osrs_p=bme280.OSRS_OVERSAMPLE_X_1, osrs_t=bme280.OSRS_OVERSAMPLE_X_1)
+        self._bme280.set_ctrl_meas_reg(mode=bme280.MODE_NORMAL, osrs_p=bme280.OSRS_OVERSAMPLE_X_1,
+                                       osrs_t=bme280.OSRS_OVERSAMPLE_X_1)
         self._bme280.set_config_reg(filter=bme280.FILTER_COEF_OFF, t_sb=bme280.STANDBY_T_1000_MS)
 
         self._bme280_awaiting_valid_measurements = True
@@ -183,7 +184,8 @@ class PebSensorPayload(SensorPayload):
         self._lsm303agr.set_accel_ctrl_reg4(accel_fs=lsm303agr.ACCEL_FS_2G, hr=0, bdu=1)  # bdu=1 needed for temperature
 
         # Magnetometer
-        self._lsm303agr.set_magneto_cfg_reg_a(magneto_odr=lsm303agr.MAGNETO_ODR_10HZ, magneto_md=lsm303agr.MAGNETO_MD_CONTINUOUS, lp=0, comp_temp_en=1)
+        self._lsm303agr.set_magneto_cfg_reg_a(magneto_odr=lsm303agr.MAGNETO_ODR_10HZ,
+                                              magneto_md=lsm303agr.MAGNETO_MD_CONTINUOUS, lp=0, comp_temp_en=1)
         self._lsm303agr.set_magneto_cfg_reg_b(lpf=1)
         self._lsm303agr.set_magneto_cfg_reg_c(bdu=1)
 
@@ -237,7 +239,6 @@ class PebSensorPayload(SensorPayload):
                 self._lsm303agr.set_magneto_cfg_reg_a(magneto_odr=lsm303agr.MAGNETO_ODR_10HZ,
                                                       magneto_md=lsm303agr.MAGNETO_MD_IDLE0, lp=0, comp_temp_en=1)
 
-
         return None
 
     def is_completed(self):
@@ -246,21 +247,40 @@ class PebSensorPayload(SensorPayload):
 
     def get_latest_data_as_bytes(self) -> bytes:
         """Get the latest data as a bytes."""
-        if self._lsm303agr_accel and self._lsm303agr_magneto:
-            # Format as packed floats
-            # https://docs.python.org/3/library/struct.html
-            packed_bytes = struct.pack("ffffffffff", self._bme280_temperature, self._bme280_pressure, self._bme280_humidity,
-                                       self._lsm303agr_accel[0], self._lsm303agr_accel[1], self._lsm303agr_accel[2],
-                                       self._lsm303agr_magneto[0], self._lsm303agr_magneto[1], self._lsm303agr_magneto[2],
-                                       self._lsm303agr_temperature, self._powermodule_vbatt)
-            # To unpack
-            # bme280_temperature, bme280_pressure, bme280_humidity, \
-            # lsm303agr_accel_x, lsm303agr_accel_y, lsm303agr_accel_z, \
-            # lsm303agr_magneto_x, lsm303agr_magneto_y, lsm303agr_magneto_z, \
-            # lsm303agr_temperature, vbatt = struct.unpack("ffffffffff", packed_bytes)
-            return packed_bytes
-        else:
-            return None
+
+        # Values - with 0.0 as default
+        bme280_temperature = float(self._bme280_temperature if self._bme280_temperature else 0.0)
+        bme280_pressure = float(self._bme280_pressure if self._bme280_pressure else 0.0)
+        bme280_humidity = float(self._bme280_humidity if self._bme280_humidity else 0.0)
+
+        lsm303agr_accel_x = float(self._lsm303agr_accel[0] if self._lsm303agr_accel else 0.0)
+        lsm303agr_accel_y = float(self._lsm303agr_accel[1] if self._lsm303agr_accel else 0.0)
+        lsm303agr_accel_z = float(self._lsm303agr_accel[2] if self._lsm303agr_accel else 0.0)
+        lsm303agr_magneto_x = float(self._lsm303agr_magneto[0] if self._lsm303agr_magneto else 0.0)
+        lsm303agr_magneto_y = float(self._lsm303agr_magneto[1] if self._lsm303agr_magneto else 0.0)
+        lsm303agr_magneto_z = float(self._lsm303agr_magneto[2] if self._lsm303agr_magneto else 0.0)
+        lsm303agr_temperature = float(self._lsm303agr_temperature if self._lsm303agr_temperature else 0.0)
+
+        vbatt = float(self._powermodule_vbatt)
+
+        # Format as packed floats
+        # https://docs.python.org/3/library/struct.html
+        # packed_bytes = struct.pack("ffffffffff", self._bme280_temperature, self._bme280_pressure, self._bme280_humidity,
+        #                           self._lsm303agr_accel[0], self._lsm303agr_accel[1], self._lsm303agr_accel[2],
+        #                           self._lsm303agr_magneto[0], self._lsm303agr_magneto[1], self._lsm303agr_magneto[2],
+        #                           self._lsm303agr_temperature, self._powermodule_vbatt)
+
+        packed_bytes = struct.pack("ffffffffff", bme280_temperature, bme280_pressure, bme280_humidity,
+                                   lsm303agr_accel_x, lsm303agr_accel_y, lsm303agr_accel_z,
+                                   lsm303agr_magneto_x, lsm303agr_magneto_y, lsm303agr_magneto_z,
+                                   lsm303agr_temperature, vbatt)
+
+        # To unpack
+        # bme280_temperature, bme280_pressure, bme280_humidity, \
+        # lsm303agr_accel_x, lsm303agr_accel_y, lsm303agr_accel_z, \
+        # lsm303agr_magneto_x, lsm303agr_magneto_y, lsm303agr_magneto_z, \
+        # lsm303agr_temperature, vbatt = struct.unpack("ffffffffff", packed_bytes)
+        return packed_bytes
 
     def get_latest_data_as_json(self):
         """Get the latest data as a json object. Which can then be loaded into json.dump/dumps."""
@@ -277,6 +297,76 @@ class PebSensorPayload(SensorPayload):
                  "vbatt": self._powermodule_vbatt}
 
         return jason
+
+    def do_calibration(self, duration=10):
+        """Do the calibration process for duration in seconds.
+        For this module it is to get the maximum and minimum values of the 3 magnetometer axes.
+        Returns the (x_min, x_max, y_min, y_max, z_min, z_max) values as int16."""
+
+        x_min = 32767
+        x_max = -32767
+        y_min = 32767
+        y_max = -32767
+        z_min = 32767
+        z_max = -32767
+
+        # Ensure the sensors are powered and the I2C pullups are enabled.
+        pyb.Pin.board.EN_3V3.on()
+        pyb.Pin('PULL_SCL', pyb.Pin.OUT, value=1)  # enable 5.6kOhm X9/SCL pull-up
+        pyb.Pin('PULL_SDA', pyb.Pin.OUT, value=1)  # enable 5.6kOhm X10/SDA pull-up
+        # i2c = machine.I2C(1, freq=400000)  # machine.I2C
+        i2c = pyb.I2C(1)  # pyb.I2C
+        i2c.init(pyb.I2C.MASTER, baudrate=400000)  # pyb.I2C
+
+        # delay
+        pyb.delay(10)
+
+        self._lsm303agr = LSM303AGR(i2c)
+
+        # Stop any previous acquisitions
+        self._lsm303agr.set_accel_ctrl_reg1(accel_odr=lsm303agr.ACCEL_ODR_POWERDOWN)
+        self._lsm303agr.set_magneto_cfg_reg_a(magneto_odr=lsm303agr.MAGNETO_ODR_10HZ,
+                                              magneto_md=lsm303agr.MAGNETO_MD_IDLE0, lp=0, comp_temp_en=1)
+
+        # delay
+        pyb.delay(10)
+
+        # Setup to take measurements
+        # Magnetometer
+        self._lsm303agr.set_magneto_cfg_reg_a(magneto_odr=lsm303agr.MAGNETO_ODR_100HZ,
+                                              magneto_md=lsm303agr.MAGNETO_MD_CONTINUOUS, lp=0, comp_temp_en=1)
+        self._lsm303agr.set_magneto_cfg_reg_b(lpf=1)
+        self._lsm303agr.set_magneto_cfg_reg_c(bdu=1)
+
+        start_time = utime.time()
+
+        while utime.time() < (start_time + duration):
+            # Check the status register for new magnetometer data
+            status_reg, xda, yda, zda, zyxda, xor, yor, zor, zyxor = self._lsm303agr.get_magneto_status_reg()
+            if zyxda:
+                x_val, y_val, z_val = self._lsm303agr.get_magneto_outputs()
+                # print("Magneto: x_val=" + str(x_val) + " y_val=" + str(y_val) + " z_val=" + str(z_val))
+
+                # Check for min and max
+                if x_val < x_min:
+                    x_min = x_val
+
+                if x_val > x_max:
+                    x_max = x_val
+
+                if y_val < y_min:
+                    y_min = y_val
+
+                if y_val > y_max:
+                    y_max = y_val
+
+                if z_val < z_min:
+                    z_min = z_val
+
+                if z_val > z_max:
+                    z_max = z_val
+
+        return x_min, x_max, y_min, y_max, z_min, z_max
 
 
 def get_sensor_payload_instance() -> SensorPayload:
